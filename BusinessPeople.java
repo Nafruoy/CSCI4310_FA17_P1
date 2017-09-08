6e6ce7f8ff6a4987afb3d2148bb597d4a3dfb8e7
madisonkh <31267036+madisonkh@users.noreply.github.com>
2017-09-08 14:32:24 -0400

    Update BusinessPeople.java
    NOT FUNCTIONAL CHANGES.

--- BusinessPeople.java
+++ BusinessPeople.java
@@ -3,6 +3,8 @@ import java.util.Random;
 public class BusinessPeople extends Persons {
 	private int BusinessNumbers;
 	private int NumberFloors;
+	private char elevatorNum;
+	private char destination;
 	
 	public void setDecisionBusiness() {
 		BusinessNumbers = super.setDecisionBusiness(3);
@@ -16,6 +18,30 @@ public class BusinessPeople extends Persons {
 	
 	public void setNumberFloors(){
 		NumberFloors = super.getFloor();
+	}
+	
+	public void setDestination(){
+		destination = Floor;
+	}
+
+	public void checkElevator(){
+		if (super.state == 'waiting'){
+		// A for loop referencing each elevator in the elevator ArrayList should be implemented for the following 3 if statements.
+			if (Elevator.onFloor == super.floor && destination.type == 'business' && (Floor.type == 'business' || Floor.type == 'worker') && Elevator.state == 'loading' && Elevator.peopleRiding < 5){
+				elevatorNum = Elevator.elevatorNum;
+				super.state= 'riding';
+			}
+			else if (Elevator.onFloor == super.floor && Elevator.type == 'worker' && destination.type == 'worker' && Elevator.state == 'loading' && Elevator.peopleRiding < 5){
+				elevatorNum = Elevator.elevatorNum;
+				super.state= 'riding';
+			}
+		else if (super.state = 'riding'){
+			if (Elevator.onFloor == destination){
+				super.floor = destination;
+				super.state = 'working';
+			}
+		}	
+		}
 	}
 	
 	public String getNumberFloor(){
